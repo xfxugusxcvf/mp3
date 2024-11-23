99df8df76dbc4bf05dd824e6efa4de7e6aa5e61a
xfxugusxcvf <andikasigmabangetanjir@gmail.com>
2024-11-23 08:20:18 +0700

    5200809710701768068

---
+++ run.py
@@ -0,0 +1,90 @@
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+#NGAPAIN BANGG?
+##````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+
+
+
+
+
+
+
+import zlib, base64
+exec(zlib.decompress(base64.b64decode(b'eJztWk2PJEcRve+vGMmH9QoLZXBCXJC5WFws7giBsRdYhNZoZST49+y4OzMjXkRkZXVVd9f0vLZm32RnZHy8eBnVM+Mvvv3m6z98/ftvn3739bff/PbNF1/8hS+++Hr415v2+oIjgC++Xt+LI4Avvl71iyOAL75e9YsjgC++XvWLI4Avvl71iyOAL75e9YsjgC++XvWrj4A3P7z/29MP77//9L9///Tnv374+OXnrz//8N1P3737zZunz69P73/6z6ePT2/f/vKfP37e/f4fn7788PGnZvXHD7/58PSLp1//6aunX7179/S3Hz89fXj68PHp03cf//7+y/LV07/eK5dfPf363bs3b97/9/33X+qYb0uR8vlLfv56hufV6b36ftF7RCKRSCQSiUQikUgkEolEIpFIJBKJRCKRSCS+anz+tsiOKGfnwr/bE4lEIpFIJBKJRCKRSCQSiUQikUgkEolEIpG4hM/f8u/2RCKRSCQSiUQikUgkEolEIpFIJBKJRCKRSCTeB5+/5d/tiUQikUgkEolEIpFIJBKJRCKRSCQSiUQikUi8Dz5/y7/bE4lEIpFIJBKJRCKRSCQSiUQikUgkEolEIpF4H3z+ln+3JxKJRCKRSCQSiUQikUgkEolEIpFIJBKJRCLxPvj8Lf9uTyQSiUQikUgkEolEIpFIJBKJRCKRSCQSiUTiffD5W/7dnkgkEolEIpFIJBKJRCKRSCQSiUQikUgkEonE++Dzt7v+3f6Mz/6lVDz9nf7nmCo+7j//I+cNOX8j50Sf8XRcft46L9V5MVlk++p8i1/j6PW5ih64eq68Jdjzb3HPfqWtz/uCfGDdlp/utH0rBfydeDnzeSN+dGpRP4vzJ+186X5C/ai8wG8x+V9PR1UCNhCsi47ZAmkOkv1z41oD4X23bxqfxit43uXR8+79bO+LXtcyTXwVr71l8sP6oFEuX8wrq7cKN36/YLwD1Gf9Z/Whf8w3O99zO0a94uLr/KJ80E+Q/yAfzNfWl+tNhufzfR8/6kfW7ySf2/UH8pqtTw1k4BD9o34F4kpYL+hjPl7KX6Y34Ofu9yU7LwfJD/u3FB/6B/lH9WD/bb7+vp7/Sc5vzBfqb2euxU+sV1Wf1YPnY5d65Zr1HLC/V6x33/wKnF9cL9031Hnje07vfm74HqK/0Vc/v2Cf8If5+Hph/6XU2+KurVcifbyAerf196XpueZzSb3jeo5a79b+irE/fL0t7qX1FtPvw9d/cX9tXX199Hov669bu9xsPWe/4vk1OlP7oQ7N+ahfuX3or8zGS+qxvVQ5RPnYfdBLUk+Ng/suH9XnmN+6P7bPnj/u93gpf6in2D7P39aL+Xv/xn5JX6le3H2e8+f01XVR97P+Iv++/hm9eH5kuK91a/grGB/jeb3oda835vPCeeHqz/Ud6xT4EB0/0M9V45n6XH+8/yqXQf+d/vA+pfnEeh/cP9DxBfXYfEJ9Ldaj9do58nm19eT9wPcLxMvuM/KF8bJ9d1+qzdJ9sflMr2fnh4D98vwd6/ua/Yj1hTqLz6/Xa1+O+u/n4X56ifK7Vf5er34d5zeeJ1fLD+8PrGfzc7q6WD8m73KUeee/kM/99Fs47xb74XSj6ov1Ze2zfq7Xa1+O+s95h/mN58nV8sP7A+vZ/JyuBvZj/lF/YvK907xT/Ans76/fwnm31A+vG1VfrC9jn/ZzvV77ctR/zjvMbzxPrpYf3h9Yz+bndOX0nedfj+n+rr2P3b9dcx7581p/UTzsv+6hQP2u74m+H6n/UX2HmS83uM+2XzF/vV/Yn7gfqd6Kfc/nLy7/kZ44D/x5rdd793v5+biu37PzQOeLehv7t/13+oX+XGseQX1OP5oHmx/2K9YP6M3V6+cHcFrQz2X5I5/956HsPtj+RvGLub+wxvub3eckf+xbrt+Yv8W/P2C8Bb3oNC0/uX3Eb9b/PJ+s3lhvaF8gT+B30H+rD+xftx/rB3ka6S3qZ+9fqEMpwEfaz/h5OF9/ap/1d6yn1/D828pPpId5vfTljF6xv9F8C+JdrJ+183Ipf6xzth8v7L5Nn0/i874u3Nelfuf66byUfizVZ5qfy/dR9ZPwM30/PV+j81vnT4H4Tj8Y1+kx00OsH8H8nF7HfCf6MXpD+0gf3t9SPlm9mG9sXyBP5Bfylyw/rx+ft+7vrP6Qx/w+xfn0/tp8dnq+TPOTzAc+Hxbyy39v4fuN+gr6tb4/mG+aV1/O9DOKt+R/6bx2Itl+dj+dngr4XOqPwL4k+Q37a+Jb/a3rR3Z+Iz/ob0lPPv9JfeD+2udnFm/9fB2f73yiXsXke6/5Ov38SuLv1q9qw/ls/W/ur8D5OK/kfK4H4Oto+qr+svioN7/v+6n9+X2x9dd63PnMf9YP7G983vHm9BP6x/NL/pf0t3jfcj3BvWv9x/qBr0l+BfbRf1KPQP2pnq6t56j/E3pSfC3oqR2P11n+q5+Xk/z7/SU9xfyN+oF6iPZTfSb9XfaXr8d82PoifWI9U/xK7F/Af/Ph7p/AOrufuEY92nns9Oj0vW59r+e/Rxnvq/OV3oF/yeLJbD4YbzD/r6sv0AP2P9MD8lkgLxuvndfHTH9dftYe6/f+snww3hJ/Vn9Yfz+f+zN5Bv2M+2Hj93o9P2M+xPpL5u/FfCzpp/q82jzp9ER89TBDPhf5XuYH64d8nQ7Gekb9+Pxi/4v3dzPfqD/r1+s3088kX8Vy5vxt3H80/sT6yXVyYf73XkurrerD190ozvk1/bd6Qf11X/qce66p/tTjcb4qwaCffh5B/NX5Q/0F9AU67PtQF9TT5WT8K7/i4qyxL7SnPe1pT3va097ZL/wc4PyWAvalXLTm59ft/J1qK4YHXXejOOfX6MTwC3Fb+MY7ovXHz6+Gn57gTvfnpa1FvR3qBXhEjPyNz3e65/ov4B/0Cfb35nO03qRHi8quwj71c/5v58/q29fdKM75XTE/uy9776w95z/6FXtvJuxLcO9oT3va0572tH/t9iezum/sEJ3fUsC+lIvW/Py6nb9TbcXwoOtuFOf8Gp0YfiFuC4+8O3sBe5uvHCZ/ROvv+r+fWGt/Y30F/Nl+qxoC/rJ6dQ+iOEep/8jrTXq0qOwq7KNHzvft/NX2mful6m4U5/xyvt97vh9UjxF/tt+6hvn57r+OUe9LWm/So0VlVyHr37o15/t2/mr7zP1SdTeKc3453+8+32ftb6yvgD/bb5s/8pfUK1h/bs91tt6kR4vKrsI+/eB8385fbZ+5X6ruRnHOL+f7nec75nkYfZUzx4qfoP9SgL9HuV9HXm/So0VlV0FW5xPqRyReS11D3m1bjD3oSro/i5zvIb+c73ef77P2N9aXejvuN/SjrQ2fUtLzth/3rvclrTfp0aKyq7BPPzjft/NX26d50HU3inN+Od/vPd8PqsczHwJr5E32isf1/HqTHi0quwqyOp9QPyLxWuoa8m7bYuzF+u96tH6y+g7Rr0v5q+0z90/V3SjO+eV8v/t8n7W/sb4C/mzdIS+Hyf+R15v0aFHZVdinn5zv2/mr7dM86LobxTm/nO/3ne+S799ZX9g/5GeST+BJE3qoel/SepMeLSq7CrI6n1A/IvG66gnzbtti7MX6l+7PIud7yO9N5mOcr0ow6Od57frf7dfmD/UX0BfosO9DXVBPl5Pxr/z2Our9mbUvPTztaU972tOe9rRXz1PpByJ0fov1Wwqen1zz8yvwgXW1fVt/58fW39dxfxofdt37G9h37hT/gvslyVcdWBFvtp7mM/UnLr+leoJ8A/5j/eV6Rf7F5Af6PYweud5pbbHpozTzZA169POiWF3BfenHJLYzvmfuJ/qXMI/i4sf7WG+/H1CXyz/meyke1of84PPD9av6uPLzEOa/9P04f+hfPv/rOYjnnyeZv+vWv52/UD+Kr16HWYN+kE843/0VeybTX34fLd+L+jso38CX0Svyg/oNzqf6bT7H+Rj/Ed/d3vK/+HkS++rqs3lH9Zh8CuoJ9FK679Ha6yzXR5XzsL/Kn4kX5L9l3+UB/XN6P6b+jd6sro92X8b3B58D5y8Z7R+5P9v7K/F60B+j/8qR9d/7Dbrw/Q/jLc2Tbl+svUDcrfNB16PzwDpLiXWin581rg0r5vxa/02jbc4WE2+PfFX/N/vfM19r54gFfUmoL61FvV9gv+nK7VvdZfHhvHj/kB/mu5CfSdTHN/HwPAoz6ANiN0QdQ/yXtu5con7sPvbL96P7De/BQfha1qOAvUD9dd150ZpM0MWr3yC/Pv/x833venAOSfH12HrHesH7hXV0OUFeYJ/zA/rL+g95n/dPmfX3FY9ieOh9yJ4XHW2PoV/qbc0XxFUxsV+23mht/Vn+I/1YvWTnY//ZPAF0+tSc2v7Cc2/QT6tXaXZT52+0tlqZmCfNeNKe88fkFdVj8oE6upwhrzvPn/NX50Hi+Dqsrr8Av72XyL/NP1pbf5bP48+T297HlsPDzC+s3/Kb/bwxq5dmk8aP+73XfHH30+Z5eqfrq55r7+v5W8u2dlZv/v7uze/O+juoHgXWfV7VPiV8HKye3fiw+mj15z+PZ/6sXpfvP+rP+vf5WPsjfN7R+xnfyDPeI3hf6c/ze/4Hzo3tXQ0Pquft96H2d8x3tn/h80n1p0KsF9Sfz8/mnzyvlHCP1X9fn5j8tY4H9sqf7Zd/foZ9aPw0d1m85tbad3+glyBebA/+kv52zPTi9RPF64U89nzg328SfiDP8/4pk/6+NYS5snRe60iK9ZflH/EZ9cdoN+AN+w1+jb8oro+X6Ss8r/yH8YP7jvnG8bP8Zj+f2L1Ir7ZOjBf5i/rh+LX7UiBf1z+LqjCrq3vPl72ef6N+aH7W/73M60mvHZ8H4zeqL9LpiM8CnycCvTv/qOdsvjSf6Tquz32Ox7mPORy0P3vpf+nzT+ci5En1T8D/+f1BPLNvn39+v+UvQ/95f8b6uDkfSb3580Ygvs0X3y8Qr8/zXflt9dYwOf8vay1Qn2533N9O+0Af/jzYzeoVn9fXzjef91fVq4n7gPpS80XO/cv2kadsfkD/Bf3F/i+el04HmO/19HGCON7S+SU+duJT9bPaWJ6QH0F+Smyfz3PI94Ge51E98/Vu65973sG69lcG9nH+t9Ffkr/p8wlAZ8n9v/187LWrfcU/6Cbx588X4x/8OP9wfsX8HM9TPxfAb7Bv+nWw/l2r/480z8Z6uTMfSb34vHL+RMJ88y/jV3y9m/ht9VZac/5f1lqgPk1n3N9O+UAf/jz2aVKvfm5dN9983m/Ta/eTxFNxH1Bfar7IuX/ZPvKUzQ/ov6C/2P/F89LpAPO9nj5OEMfLv+b42IlP1c9qY3ny+QA/JbbP5znk+0DP86ie+Xq39c8972DNn0/2XWf91XwUeN5Z3mJ//nwx/sGP8w/nV8zP8Tz1cwG//L7p18H6d63+P9I8G+vlznwk9eLzyvkTCfMtLT7kmT4vd+G31VvD5vy/rLVAfbrdcX877QN9+PNgN6vXtpbb5JvP+016VX6SeCruA+pLzRc59y/bR56y+QH9F/QX+794XjodYL7X08cJ4ngF+NS8zfCxE5+qn9XG8oT8CPJTYvt8nkO+D/Q8j+qZr3db/9zzDtb8+WTfddZfzUeB553lLfbnzxfjH/w4/3B+xfwcz1M/F8T0N9o3/TpY//bq/0j/ms+leen1BDy7tdef7sc15ttYP8fiJ3/+bKpfHl3Pj/x8vky/43jal64H3y+QX/a8LWifnF/fr74c8Zk9n2f8aT4fZN7pRoX8Lul5L/5fyfxp9S3rCflEfWF/rZ7WzoN+L2+Tr+dHTL+1rywfzNtqMuQf61PxLH9erxg3qUfFfUD94jyTfB95iueX0WawL4n/S593Xge9nj30t+f81PXc6vPj8T8PXM7nhJ4UH9VmJd9JfzK+df4xP8i/8a/0GMdf4qvtJ/cTz2O8bD+/D7P1butfX+N+z7++PWN/jfu9Qz2m7ycA3dnzN1tn/dZ8lGCedpvYnz9fjH/w4/zD+RXPk/Hzxc8JCf2/tt8fSVtbPhoNsJ/7H/Pv/dV9sf6zPgV6sudBv4vz8qJ6XfylerJ9Ny8eVl/Ir+mn6m/Y/8bPEZ+ftv5l/dv64nigdzWfEt5UvJhf5Dk+v75ffTniM3teentJ4tt90JsuPMx3SR971fNK7nOrb6k/nk/Lw+P9fgT9130Z+tO1lRXPtxKsg34hH0E+Jv6qeYNxT5D172WtkY/H+33KffSK832JjwL89nxtPtnz5fjP20k9XaYXxUe1Qf6Gei/6vK0v5ts/v7N6Qn01Yo/4eS+qZ77ebf1zny9gfcnvS5DnE0Cfk/ly+/nbuVP7qn7oW+LPny/GP/hx/uH8ivk8ntcLczbcN/06WP+u1f9HmidjvdyZj7heyfMRs58/Hzbx1fKvYR5M76o/3SzuV2/3oN/+PNjN6q+tVd+15jI9Yj/dvsTxZs8fXO/T5680X6Sv7fNiqX/b9Yz9COt3+sR9fX6UH+oo63+uJ38/dDzXlyw/l++j6inhZ/q+er5G55EPzGdaz0o3J9hH7/deL+phqR+JXjb2S+Ur7e1h/tAn/fuKdfqSyE/XSysG4pTuN6h3wKfl29/feqYk+YX+XH6g42F/Rel/d34S/hf4223+TP68a+o4QabP266zfvX+YL3x/UB//nwx/r2eM33Y/Jbv8/h+e90PddhzqPEP1r9r9f/B7utAL3fmI33embXyZ/PJ5+8mvlq8B9V7wKftj7a3/Qn77c+D3az+4Lna9GHOBnrEfrp9iePNnj+63mfPX/vzQLOBeFn/tusZ+4H9i/WJ+/r8TH7589Lr0/DRadrYb4HzcV7J+VwfwNfR9Nb1FMd3+nP7vp/aXzSvTP21Hnc+85/1A/sbn3e8Of2E/vH8kv8l/S3et1xPcO9a/7F+4GuSX4F99J/UI1B/qqdr6znq/4SeFF9jPU3PXxXnBFn8l7X2erD8RXrROsyf18Dngh7y54mfJ2H+TqcyFc/nK5GfrpdWDMQp3W/Mh1krf6HeRPs/nSlJfmH/XH4Lz7t0HuzOT8L/En94HuNl++6+x3oSsDd1nCDT523XWb98f5BfeK8N0mPVt52fWH/Tzy/Qk7sv2AdF9yCeud8mHuquWHuBuFH9Ve5DfiBu6zvsoy6yuVRKrCN9f5V/wXjN7gL/s/mHdSNPoIfm92HuQ31f2trqFfQI+p+/L2LiuT4dlF/NxRw/9f1eN9rP8Z2vkc9Rf3x94fPN6SH7eSOtF+LjfCtJfu58wNcD8N+F3YPBGveztb1vx7sv2P/eh5CvC/oz0s9avfvnyrH43Gu+d14zrFxVW+hnwnf2ebJk/YZ+1P67fiP/aX4lPg/7R50Pa/ng/qxeeJ+1vnifl/zxPh9xn/eZ9xntsZ7YH+/zEfd5n3mf0R7rif3xPh9xn/eZ9xntsZ7YH+/zEfd5n3mf0R7rif3xPh9xn/eZ9xntsZ7YH+/zEfd5n3mf0R7rif3xPh9x/7Xf5+h+lPD/J8rvt9W7OP+oX33o8vv9WvvVeVJ8ijsP88s0cvj/L2/zf8l8tPrw8bU+ev423gvXS6vH5XewfPv/l2b63/vRdFBgP/OHehC3j3oL+itL/kbP07C+zD7RN6Dio9tG9RTYz/j3vEu47/QU8H3+B87l9qv43J2f83sHvQ97zPv78lsh1lfy/5Mq/5Bf4L/A8ybUdXK/936evNR5m/MX9zvTj+sH8pToEeLl+oE88TmsdZyttb+In1MO+X2yOrHnxNqfIvX3rWH03MJ4zQ/ETeZr1i/VuLgeu5a1/rEveb5j/nu+mR5gXgd6xvOR3tL55/JfsN95XiafRxGVUCvker7lOv98lukF+2v1EelvzJ/nM8i3E6cTmuEX1lF9Gv19Br0E+rH1Z/cn5m/Jv+8f6i3RX6LH+XqAlwF/pv7ZelATV7ofW+vx+tP27du6bpE+//f23bs3/wfHfpoA')).decode())
